ae409479df35895aa256975d64384fd9709beec8
Mateusz Moroszczuk <moro143moro@gmail.com>
2022-03-28 13:22:26 +0200

    Across actives working (Go tile)

--- classes/player.py
+++ classes/player.py
@@ -14,3 +14,14 @@ class Player:
             self.current_place -= max_place
         ending_point =self.current_place
         
+        for i in parent.across_active:
+            if starting_point<ending_point:
+                if starting_point < i and i >= ending_point:
+                    active = parent.tiles[i].across_active.split('*')
+                    if active[0] == 'Give':
+                        self.money += int(active[1])
+            else:
+                if starting_point < i or i <= ending_point:
+                    active = parent.tiles[i].across_active.split('*')
+                    if active[0] == 'Give':
+                        self.money += int(active[1])
